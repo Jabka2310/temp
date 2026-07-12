cc199d0f5fa9cd1ef48fa771beae8d67d197ce08
Your Name <you@example.com>
2026-07-12 22:40:57 +0300

    web.mdel

---
+++ TestBackSQL/src/main/java/tictactoe/web/model/CurrentGameDto.java
@@ -0,0 +1,25 @@
+package tictactoe.web.model;
+
+import java.util.UUID;
+
+public class CurrentGameDto {
+    private UUID id;
+    private GameBoardDto board;
+    private Boolean computerStarts;
+
+    public CurrentGameDto(){}
+
+    public CurrentGameDto (UUID id, GameBoardDto board) {
+        this.id = id;
+        this.board = board;
+    }
+
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+
+    public GameBoardDto getBoard() { return board; }
+    public void setBoard(GameBoardDto board) { this.board = board; }
+
+    public Boolean getComputerStarts() { return computerStarts; }
+    public void setComputerStarts(Boolean computerStarts) { this.computerStarts = computerStarts; }
+}
